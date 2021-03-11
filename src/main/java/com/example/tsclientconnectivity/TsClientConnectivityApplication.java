9dd1cdd21b65a13500385cb49371b29b36a8c145
penbosso <ckbosso@st.ug.edu.gh>
2021-03-11 15:14:25 +0000

    initial setup

---
+++ src/main/java/com/example/tsclientconnectivity/TsClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsclientconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsClientConnectivityApplication.class, args);
+	}
+
+}
