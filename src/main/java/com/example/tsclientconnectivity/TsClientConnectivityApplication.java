266401afda02b8c2b61316589d8b7a34b06aa89a
penbosso <ckbosso@st.ug.edu.gh>
2021-03-15 15:25:47 +0000

    implementation of order soap consumption, client connectivity can communicate with ordation now

--- src/main/java/com/example/tsclientconnectivity/TsClientConnectivityApplication.java
+++ src/main/java/com/example/tsclientconnectivity/TsClientConnectivityApplication.java
@@ -1,12 +1,28 @@
 package com.example.tsclientconnectivity;
 
+import com.example.tsclientconnectivity.client.SoapClient;
+import com.example.tsclientconnectivity.ordervalidation.Acknowledgement;
+import com.example.tsclientconnectivity.ordervalidation.OrderRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TsClientConnectivityApplication {
 
-	public static void main(String[] args) {
+	@Autowired
+	private SoapClient client;
+
+	@PostMapping("/submitorder")
+	public Acknowledgement invokeSoapClientToSubmitClientOrder(@RequestBody OrderRequest request) {
+		return client.submitOrder(request);
+	}
+		public static void main(String[] args) {
 		SpringApplication.run(TsClientConnectivityApplication.class, args);
 	}
 
