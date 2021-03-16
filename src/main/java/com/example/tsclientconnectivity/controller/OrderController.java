2de9a8bec447f1c00628c37863a064c758950f91
Nunoo Prince Harry <tURNTABL+computer-mann@users.noreply.github.com>
2021-03-16 16:34:34 +0000

    Added the isAdmin prop to order

--- src/main/java/com/example/tsclientconnectivity/controller/OrderController.java
+++ src/main/java/com/example/tsclientconnectivity/controller/OrderController.java
@@ -1,23 +1,21 @@
-package com.example.tsclientconnectivity;
+package com.example.tsclientconnectivity.controller;
+
 
 import com.example.tsclientconnectivity.client.SoapClient;
 import com.example.tsclientconnectivity.ordervalidation.Acknowledgement;
 import com.example.tsclientconnectivity.ordervalidation.OrderRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class TsClientConnectivityApplication {
+public class OrderController {
+    @Autowired
+    private SoapClient client;
 
-
-		public static void main(String[] args) {
-		SpringApplication.run(TsClientConnectivityApplication.class, args);
-	}
-
+    @PostMapping("/submitorder")
+    public Acknowledgement invokeSoapClientToSubmitClientOrder(@RequestBody OrderRequest request) {
+        return client.submitOrder(request);
+    }
 }
